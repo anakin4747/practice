42412448f7c27380c8fb350f6736bfdaaf0adbf0
Anakin Childerhose <anakin@childerhose.ca>
2022-02-15 22:20:36 -0500

    New exxerxssisx

---
+++ ExcersizeProgram.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+now = datetime.now()
+
+class Excersize:
+
+
+    def __init__(self, frequency, frequency_daily):
+        self.frequency = frequency
+        self.frequency_daily = frequency_daily
+
+
+    def excersizeReady(self, excersize):
+        pass # takes an excersize and tells you if its ready
+             # should be able to wait for 3 days so you will have to access current time 
+
+
